7c790aade50201a952dc57e1d5efb57e975a886f
denis1krivtsov <51832227+denis1krivtsov@users.noreply.github.com>
2020-03-15 21:52:17 +0200

    finish task 8_2 & 8_3

--- Lab8QuickSort/8_2task/user.py
+++ Lab8QuickSort/8_2task/user.py
@@ -3,7 +3,7 @@
 Реалізуйте підпрограму сортування масиву.
 """
 
-N = 10000  # Кількість елементів масиву.
+N = 1000000  # Кількість елементів масиву.
              # Використовується у головній програмі для генерування масиву з випадкових чисел
              # Для повільних алгоритмів сортування з асимптотикою n**2 рекомендується
              # використовувати значення не більше 10к
@@ -11,10 +11,36 @@ N = 10000  # Кількість елементів масиву.
              # nlog(n) встановіть значення 1 000 000
 
 
-def sort(array):
+
+def qsort(array, a, b):
     """ Сортування масиву
     :param array: Вхідний масив даних, що треба відсортувати.
     """
-    pass  # TODO: реалізуйте відповідний алгоритм тут
+    if a >= b:
+        return
+    pivot = array[a + (b - a)//2]
+    left = a
+    right = b
 
+    while True:
+        while array[left] < pivot:
+            left += 1
+
+        while array[right] > pivot:
+            right -= 1
+
+        if left >= right:
+            break
+        array[left], array[right] = array[right], array[left]
+        left += 1
+        right -= 1
+    qsort(array, a, right)
+    qsort(array, right + 1, b)
+    pass# TODO: реалізуйте відповідний алгоритм тут
+
+def sort(array):
+    qsort(array, 0, len(array) - 1)
+    pass
+# arr = [1, 3, -10, 55, 1000, 10, 10, 2]
+# print(qsort(arr, 0, len(arr)-1))
 
--- Lab8QuickSort/8_3task/user.py
+++ Lab8QuickSort/8_3task/user.py
@@ -3,7 +3,7 @@
 Реалізуйте підпрограми сортування масиву.
 """
 
-N = 10000     # Кількість елементів масиву.
+N = 10000    # Кількість елементів масиву.
               # Використовується у головній програмі для генерування
               # масиву з випадкових чисел
 
@@ -13,6 +13,14 @@ def bubble_sort(array):
 
     :param array: Масив (список однотипових елементів)
     """
+    n = len(array)
+    k = 0
+    for i in range(n-1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j+1]:
+                k += 1
+                array[j], array[j+1] = array[j+1], array[j]
+
     pass  # TODO: реалізуйте відповідний алгоритм тут
 
 def bubble_sort_optimized(array):
@@ -20,7 +28,19 @@ def bubble_sort_optimized(array):
 
     :param array: Вхідний масив даних, що треба відсортувати.
     """
-    pass  # TODO: реалізуйте відповідний алгоритм тут
+    n = len(array)
+    k = 0
+    for i in range(n - 1, 0, -1):
+        sortCheck = 0
+        for j in range(i):
+            if array[j] > array[j + 1]:
+                k += 1
+                array[j], array[j + 1] = array[j + 1], array[j]
+            else:
+                sortCheck += 1
+        if sortCheck == i:
+            break
+    pass # TODO: реалізуйте відповідний алгоритм тут
 
 
 def selection_sort(array):
@@ -29,7 +49,14 @@ def selection_sort(array):
     :param array: Масив (список однотипових елементів)
     :return: None
     """
-    pass  # TODO: реалізуйте відповідний алгоритм тут
+    n = len(array)
+    for i in range(n-1, 0, -1):
+        currentMax = 0
+        for j in range(1, i+1):
+            if array[currentMax] < array[j]:
+                currentMax = j
+        array[i], array[currentMax] = array[currentMax], array[i]
+    pass
 
 
 def insertion_sort(array):
@@ -38,6 +65,18 @@ def insertion_sort(array):
     :param array: Масив (список однотипових елементів)
     :return: None
     """
+    n = len(array)
+    # TODO: реалізуйте відповідний алгоритм тут
+    for i in range(1, n):
+        currentValue = array[i]
+        position = i
+        while position > 0:
+            if array[position - 1] > currentValue:
+                array[position] = array[position - 1]
+            else:
+                break
+            position -= 1
+        array[position] = currentValue
     pass  # TODO: реалізуйте відповідний алгоритм тут
 
 
@@ -47,9 +86,62 @@ def merge_sort(array):
     :param array: Масив (список однотипових елементів)
     :return: None
     """
+    if len(array) > 1:
+        mid = len(array) // 2
+        leftArr = array[:mid]
+        rightArr = array[mid:]
+        merge_sort(leftArr)
+        merge_sort(rightArr)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(leftArr) and j < len(rightArr):
+            if leftArr[i] < rightArr[j]:
+                array[k] = leftArr[i]
+                i += 1
+            else:
+                array[k] = rightArr[j]
+                j += 1
+            k += 1
+
+        while i < len(leftArr):
+            array[k] = leftArr[i]
+            i += 1
+            k += 1
+
+        while j < len(rightArr):
+            array[k] = rightArr[j]
+            j += 1
+            k += 1
     pass  # TODO: реалізуйте відповідний алгоритм тут
 
 
+def qsort(array, a, b):
+    """ Сортування масиву
+    :param array: Вхідний масив даних, що треба відсортувати.
+    """
+    if a >= b:
+        return
+    pivot = array[a + (b - a)//2]
+    left = a
+    right = b
+
+    while True:
+        while array[left] < pivot:
+            left += 1
+
+        while array[right] > pivot:
+            right -= 1
+
+        if left >= right:
+            break
+        array[left], array[right] = array[right], array[left]
+        left += 1
+        right -= 1
+    qsort(array, a, right)
+    qsort(array, right + 1, b)
+    pass# TODO: реалізуйте відповідний алгоритм тут
 
 def quick_sort(array):
     """ Швидке сортування
@@ -57,6 +149,6 @@ def quick_sort(array):
         :param array: Масив (список однотипових елементів)
         :return: None
         """
+    qsort(array, 0, len(array) - 1)
     pass  # TODO: реалізуйте відповідний алгоритм тут
-
 
