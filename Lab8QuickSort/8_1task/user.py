5ea72b873bd8417e8501c169072afc66cba094cd
denis1krivtsov <51832227+denis1krivtsov@users.noreply.github.com>
2020-03-15 20:49:38 +0200

    finish task 8_1

--- Lab8QuickSort/8_1task/user.py
+++ Lab8QuickSort/8_1task/user.py
@@ -3,7 +3,7 @@
 Реалізуйте підпрограму сортування масиву.
 """
 
-N = 10000  # Кількість елементів масиву.
+N = 1000000  # Кількість елементів масиву.
              # Використовується у головній програмі для генерування масиву з випадкових чисел
              # Для повільних алгоритмів сортування з асимптотикою n**2 рекомендується
              # використовувати значення не більше 10к
@@ -15,6 +15,33 @@ def sort(array):
     """ Сортування масиву
     :param array: Вхідний масив даних, що треба відсортувати.
     """
-    pass  # TODO: реалізуйте відповідний алгоритм тут
+    if len(array) > 1:
+        mid = len(array) // 2
+        leftArr = array[:mid]
+        rightArr = array[mid:]
+        sort(leftArr)
+        sort(rightArr)
 
+        i = 0
+        j = 0
+        k = 0
+        while i < len(leftArr) and j < len(rightArr):
+            if leftArr[i] < rightArr[j]:
+                array[k] = leftArr[i]
+                i += 1
+            else:
+                array[k] = rightArr[j]
+                j += 1
+            k += 1
+
+        while i < len(leftArr):
+            array[k] = leftArr[i]
+            i += 1
+            k += 1
+
+        while j < len(rightArr):
+            array[k] = rightArr[j]
+            j += 1
+            k += 1
+    pass# TODO: реалізуйте відповідний алгоритм тут
 
